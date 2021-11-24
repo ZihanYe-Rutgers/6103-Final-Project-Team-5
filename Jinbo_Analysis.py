ef5e4482e1d659d6d8f9c3c8121201dc75e5819a
Jinbo-Li919 <jinbo_li@gwu.edu>
2021-11-24 16:53:56 -0500

    Update Jinbo_Analysis.py

--- Jinbo_Analysis.py
+++ Jinbo_Analysis.py
@@ -9,6 +9,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix 
 from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.tree import export_graphviz  
 
 
 #%%
@@ -60,6 +62,7 @@ print("The training data size is : {} ".format(x_train.shape))
 print("The test data size is : {} ".format(x_test.shape))
 
 # %%
+#tree with all features
 clf = DecisionTreeClassifier()
 clf.fit(x_train, y_train)
 # train set
@@ -87,7 +90,7 @@ def cv_score(d):
     clf.fit(x_train, y_train)
     return(clf.score(x_train,y_train), clf.score(x_test,y_test))
 x_train, x_test, y_train, y_test = train_test_split (x, y, test_size=0.2, random_state=1)
-depths = np.arange(1,20)
+depths = np.arange(1,10)
 scores = [cv_score(d) for d in depths]
 tr_scores = [s[0] for s in scores]
 te_scores = [s[1] for s in scores]
@@ -96,3 +99,49 @@ te_best_index = np.argmax(te_scores)
 
 print("bestdepth:", te_best_index+1, "bestdepth_score:", te_scores[te_best_index])
 # %%
+# best depth plot
+depths = np.arange(1,10)
+plt.figure(figsize=(6,4), dpi=120)
+plt.grid()
+plt.xlabel('max depth of decison tree')
+plt.ylabel('Scores')
+plt.plot(depths, te_scores, label='test_scores')
+plt.plot(depths, tr_scores, label='train_scores')
+plt.legend()
+
+
+# %%
+# best depth tree
+clf2 = DecisionTreeClassifier(max_depth=9)
+clf2 = clf2.fit(x_train, y_train)
+
+print("train set evaluation:")
+print("score:", clf2.score(x_train, y_train))
+print("Confusion Matrix: \n",confusion_matrix(y_train, clf2.predict(x_train)))
+print("Classification report:\n",classification_report(y_train, clf2.predict(x_train)))
+
+#%%
+print("test set evaluation:")
+print("score:", clf2.score(x_test, y_test))
+print("Confusion Matrix: \n",confusion_matrix(y_test, clf2.predict(x_test)))
+print("Classification report:\n",classification_report(y_test, clf2.predict(x_test)))
+
+
+#%%
+# Most important features
+features = cols.columns
+importances = clf2.feature_importances_
+leading_indices = (-importances).argsort()[:6]
+print ("Features sorted by importance:")
+for i in range (6):
+    print (i+1, features[leading_indices[i]], round(100*importances[leading_indices[i]],2), '%')
+
+#%%
+# Save the tree dot
+with open("tree.dot", 'w') as file:
+    file = tree.export_graphviz(clf2, out_file=file)
+
+
+
+
+#%%
